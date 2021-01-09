4ffa35f95dfd81a8852477520774df193cea849f
long <wl885683@163.com>
2021-01-09 15:39:10 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/login"></router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "./styles/normalize.css";
+@import "./styles/main.scss";
+</style>
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="login">登录</div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+#login {
+  width: 100vw;
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
