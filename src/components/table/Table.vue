ecbb68dc334e9447483a3e40c7368f51ce4de733
long <wl885683@163.com>
2021-02-20 23:59:23 +0800

    el-table

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -1,8 +1,75 @@
 <template>
-  <div>十点5分</div>
+  <el-table :data="dataSet.tableData" border style="width:100%">
+    <!-- 多选框 -->
+    <el-table-column
+      v-if="dataSet.tableConfig.selection"
+      type="selection"
+    ></el-table-column>
+    <!-- 文本数据渲染 -->
+    <el-table-column
+      v-for="item in dataSet.tableConfig.tHead"
+      :key="item.field"
+      :prop="item.field"
+      :label="item.label"
+      :width="item.width"
+    ></el-table-column>
+  </el-table>
 </template>
 <script>
+import { onBeforeMount, reactive } from "@vue/composition-api";
 export default {
-  name: "Table"
+  name: "Table",
+  props: {
+    config: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  setup(props) {
+    const dataSet = reactive({
+      tableConfig: {
+        selection: true,
+        recordCheckbox: false,
+        tHead: []
+      },
+      tableData: [
+        {
+          email: "asd",
+          name: "王小虎",
+          phone: 11,
+          address: "ada",
+          role: "上海萨达"
+        },
+        {
+          email: "asd",
+          name: "王小虎",
+          phone: 111,
+          address: "ada",
+          role: "上海萨达"
+        }
+      ]
+    });
+    /*
+     * 方法
+     */
+    const initTableConfig = () => {
+      let configData = props.config;
+      for (let key in configData) {
+        if (dataSet.tableConfig[key]) {
+          dataSet.tableConfig[key] = configData[key];
+        }
+      }
+    };
+    onBeforeMount(() => {
+      initTableConfig();
+    });
+
+    return {
+      dataSet,
+      // 方法
+      initTableConfig
+    };
+  }
 };
 </script>
+<style lang="scss" scoped></style>
--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -10,20 +10,40 @@
         <el-button type="danger" class="pull-right">新增用户</el-button>
       </el-col>
     </el-row>
+    <div class="table-data">
+      <table-data :config="dataSet.configTable"></table-data>
+    </div>
   </div>
 </template>
 <script>
 import { reactive } from "@vue/composition-api";
 import SearchSelect from "@/components/select/SearchSelect";
+import TableData from "@/components/table/Table";
 export default {
   name: "User",
   components: {
-    SearchSelect
+    SearchSelect,
+    TableData
   },
 
   setup() {
     const dataSet = reactive({
-      configOption: { init: ["name", "phone", "email"] }
+      configOption: { init: ["name", "phone", "email"] },
+      // table组件配置
+      configTable: {
+        //多选框
+        selection: false,
+        // 翻页记录
+        recordCheckbox: true,
+        // 表头
+        tHead: [
+          { label: "用户名", field: "email", width: "150" },
+          { label: "真实姓名", field: "name", width: "120" },
+          { label: "手机号", field: "phone", width: "150" },
+          { label: "地区", field: "address" },
+          { label: "角色", field: "role", width: "120" }
+        ]
+      }
     });
     return { dataSet };
   }
@@ -37,5 +57,9 @@ export default {
 
 .hide {
   opacity: 0;
+}
+.table-data {
+  margin-top: 30px;
+  text-align: center;
 }
 </style>
